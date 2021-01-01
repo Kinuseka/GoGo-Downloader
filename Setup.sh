072242d6cadb22c4e0a50ebed682a3c4b45d436d
Kinuseka <66762783+Kinuseka@users.noreply.github.com>
2021-01-01 20:22:41 +0800

    update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -7,10 +7,14 @@ if checker bash; then
   echo "Command already setup"
 else
   echo >$PREFIX/bin/goanime
+  echo >$PREFIX/bin/goupdate!
   chmod +x $PREFIX/bin/goanime
-  echo "cd $HOME/storage/shared/GoGo-Downloader && python RuNime.py" 
+  chmod +x $PREFIX/bin/goupdate!
+  echo "cd $HOME/storage/shared/GoGo-Downloader && python RuNime.py" >> $PREFIX/bin/goanime
+  echo "wget -O '-https://raw.githubusercontent.com/Kinuseka/GoGo-Downloader/main/Setup.sh'" >> $PREFIX/bin/goupdate!
 installer()
 {
   cd "$HOME/storage/shared/GoGo-Downloader"
-  command wget ''
+  command wget 'https://github.com/Kinuseka/GoGo-Downloader/releases/download/V1.2-prerelease/GoGoDownloader.v1.2pre.zip'
+  command unzip -o 'GoGoDownloader v1.2.pre.zip'
 }
