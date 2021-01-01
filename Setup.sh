93b9bd5c616895cd0c0067dc2b0a9a274a0117a6
Kinuseka <66762783+Kinuseka@users.noreply.github.com>
2021-01-01 22:32:01 +0800

    update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -24,6 +24,7 @@ rinnstaller()
   echo >$PREFIX/bin/goanime
   chmod +x $PREFIX/bin/goanime
   echo "cd $HOME/storage/shared/GoGo-Downloader && python RuNime.py" >> $PREFIX/bin/goanime
+  echo ">>Setupped 001"
 }
 rimeinstaller()
 {
