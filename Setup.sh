08b211ec1de01a23ec681906df3a6f5b04d301a8
Kinuseka <66762783+Kinuseka@users.noreply.github.com>
2021-03-20 19:41:04 +0800

    update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -214,4 +214,5 @@ printf 'Do "gouninstall" to uninstall\n'
 printf 'Do "goupdate" to update!\n'
 printf '======================================
 Setup finished! do "goanime" to start!
-======================================'
+======================================
+'
